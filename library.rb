5d8756237441c5e50956c72ee0227536e5e57b42
Pat <graczyk.pat@gmail.com>
2018-07-02 22:21:04 +0100

    files set up, first activities and a method

--- library.rb
+++ library.rb
@@ -1,3 +1,41 @@
-class MyLibrary
+class Library
 
-end 
+attr_reader :books
+
+def initialize
+
+  @books = [
+    {
+      title: "lord_of_the_rings",
+      rental_details: {
+        student_name: "Jeff",
+        date: "01/12/16"
+      }
+    },
+    {
+      title: "tile_book_2",
+      rental_details: {
+        student_name: "Anna",
+        date: "10/12/16"
+      }
+    },
+    {
+      title: "tile_book_3",
+      rental_details: {
+        student_name: "Molly",
+        date: "10/12/16"
+      }
+    }
+  ]
+
+  end
+
+def book_details(check_title)
+    for book in @books[:title]
+     if book == check_title
+       return[:rental_details]
+  end
+end
+
+end
+end
--- specs/library_specs.rb
+++ specs/library_specs.rb
@@ -4,4 +4,44 @@ require('minitest/rg')
 
 class TestLibrary < MiniTest::Test
 
+def setup
+
+  @books = [
+    {
+      title: "lord_of_the_rings",
+      rental_details: {
+        student_name: "Jeff",
+        date: "01/12/16"
+      }
+    },
+    {
+      title: "tile_book_2",
+      rental_details: {
+        student_name: "Anna",
+        date: "10/12/16"
+      }
+    },
+    {
+      title: "tile_book_3",
+      rental_details: {
+        student_name: "Molly",
+        date: "10/12/16"
+      }
+    }
+  ]
+  end
+
+def test_book_details
+  my_library = Library.new(books)
+  my_library.book_details("lord_of_the_rings")
+  assert_equal(:rental_details, book_details)
 end
+
+
+end
+
+#1. Create a class for a Library that has an array of books
+#done
+
+#2.Create a getter for the books
+#done
