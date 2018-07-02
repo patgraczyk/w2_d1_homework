bf13e8ccd9b42f3c51a42a197850cfe2f79a942e
Pat <graczyk.pat@gmail.com>
2018-07-02 21:17:32 +0100

    another activity

--- football.rb
+++ football.rb
@@ -1,7 +1,7 @@
 class FootballTeam
 
-attr_reader :team_name, :players, :coach
-attr_writer :team_name, :players, :coach
+attr_reader :team_name, :players, :coach, :points
+attr_writer :team_name, :players, :coach, :points
 
 def initialize (team_name, players, coach)
   @team_name = team_name
@@ -9,9 +9,19 @@ def initialize (team_name, players, coach)
   @coach = coach
 end
 
+
 def add_players(new_player)
     @players.push(new_player)
 end
 
+# def player_in_players(check_player)
+#   if @players == check_player
+#   return true
+# end
+# end
+# end
 
+# def test_team_scores(result)
+#   @points +=1 if ("win")
+# end
 end
--- specs/football_spec.rb
+++ specs/football_spec.rb
@@ -10,6 +10,12 @@ class TestStudents < MiniTest::Test
     assert_equal("Lech", football_team.team_name)
   end
 
+  def test_coach_name
+    football_team = FootballTeam.new("Lech", ["One", "Two", "Three"], "Graczyk")
+    football_team.coach="Morris"
+    assert_equal("Morris", football_team.coach)
+  end
+
 #Create a method that adds a new player to the players array.
   def test_add_players
     football_team = FootballTeam.new("Lech", ["One", "Two", "Three"], "Graczyk")
@@ -20,9 +26,22 @@ class TestStudents < MiniTest::Test
 #Add a method that takes in a string of a player's name and checks
 #to see if they are in the players array.
 
+# def test_player_in_players
+#   football_team = FootballTeam.new("Lech", ["One", "Two", "Three"], "Graczyk")
+#   football_team.players("One")
+#   assert_equal(true, football_team.players)
+# end
+
 #Add a points property into your class that starts at 0.
+#DONE
 
 #Create a method that takes in whether the team has won or
 #lost and updates the points property for a win.
+#
+# def test_team_scores
+#   football_team = FootballTeam.new("Lech", ["One", "Two", "Three"], "Graczyk")
+#   football_team.points("win")
+#   assert_equal(1, football_team.points)
+# end
 
 end
